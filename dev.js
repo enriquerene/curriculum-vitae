98b7c69ad1517ba38844242e031b0c8d1a6be9dd
Enrique René Beauxis Reyes <enriquerenebr@gmail.com>
2024-01-28 09:26:10 -0300

    node and react profile

--- dev.js
+++ dev.js
@@ -1,18 +1,15 @@
-var fs = require('fs');
-var { exec } = require('child_process');
+var fs = require("fs");
+const sass = require("sass");
 
-fs.watch('./sass', function(event, filename) {
+fs.watch("./sass", function (event, filename) {
   if (filename) {
     if (filename.match(/\.scss$/)) {
-      exec('sass sass/main.scss assets/css/main.css', function(err, stdout, stderr) {
+      const result = sass.compile("./sass/main.scss");
+      fs.writeFile("./assets/css/main.css", result.css, (err) => {
         if (err) {
-          console.log('error:' + err.message);
-          return;
+          console.error(err);
         }
-        if (stderr) {
-          console.log('stderr:' + stderr);
-          return;
-        }
+        console.log("CSS file updated successfully");
       });
     }
   }
